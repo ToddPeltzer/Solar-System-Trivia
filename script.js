3bb340378e6cdd5e301c25403f9c1bcf4f69aa2b
toddpeltzer <toddpeltzer14@gmail.com>
2021-07-29 16:32:02 -0700

    was able to get from home page to mercury question page with a button click

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-// let answers = document.querySelectorAll('.button') //grabbing all answer buttons
+let welcomeDiv = document.querySelector('.welcomeSection')
+let welcomeTitle = document.querySelector('.welcome')
+let welcomeStatement = document.querySelector('.welcomeStatement')
+let startButton = document.querySelector('.startGame')
+let questionDiv = document.querySelector('.questionSection')
 let planet = document.querySelector('.planetName') //grabbing planet name to be displayed
 let question = document.querySelector('.planetQuestion') //grabbing question to be displayed
 //grabbing all answer buttons
-let answer1 = document.querySelector("#answer1").style.display = "none"
-let answer2 = document.querySelector("#answer2").style.display = "none"
-let answer3 = document.querySelector("#answer3").style.display = "none"
-let answer4 = document.querySelector("#answer4").style.display = "none"
+let allButtons = document.querySelectorAll('.button')
+let answer1 = document.querySelector("#answer1")
+let answer2 = document.querySelector("#answer2")
+let answer3 = document.querySelector("#answer3")
+let answer4 = document.querySelector("#answer4")
 
 //planet information
 const planetInformation = [
@@ -21,16 +26,26 @@ const planetInformation = [
 
 
 //opening page
-document.querySelector('.welcome').innerText = "Welcome to Solar System Trivia"
-document.querySelector('.welcomeStatement').innerText = "Do you have what it takes to go to the great beyond?"
-document.querySelector('.startGame').innerText = "Start Game"
+welcomeTitle.innerText = "Welcome to Solar System Trivia"
+welcomeStatement.innerText = "Do you have what it takes to go to the great beyond?"
+startButton.innerText = "Start Game"
+questionDiv.style.visibility = 'hidden'
 
 
-//opening page start if in the game
-function startPage() {
-    let welcome = document.querySelector('.planetName')
-    welcome.innerText = "Welcome to Solar System Trivia" 
+//function to get to mercury from home page
+function welcomePageToMercury () {
+    welcomeDiv.style.visibility = 'hidden'
+    planet.innerText = 'Mercury'
+    question.innerText = 'this is a question?'
+    answer1.innerHTML = 'test1'
+    answer2.innerText = 'test2'
+    answer3.innerText = 'test3'
+    answer4.innerText = 'test4'
+    questionDiv.style.visibility = 'visible'
 }
+
+//button to start the game function/event
+startButton.addEventListener('click', welcomePageToMercury)
 
 
 
