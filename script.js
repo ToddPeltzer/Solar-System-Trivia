90265fa99678adeae39142cc6291f3d6a53c3380
toddpeltzer <toddpeltzer14@gmail.com>
2021-07-31 19:47:00 -0700

    Added images for each question

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let welcomeStatement = document.querySelector('.welcomeStatement')
 let startButton = document.querySelector('.startGame')
 let questionDiv = document.querySelector('.questionSection')
 let planet = document.querySelector('.planetName') //grabbing planet name to be displayed
+let planetImage = document.querySelector('.planetImage')
 let question = document.querySelector('.planetQuestion') //grabbing question to be displayed
 //grabbing all answer buttons
 let allButtons = document.querySelectorAll('.button')
@@ -16,15 +17,17 @@ let counter = 0
 
 //planet information
 const planetInformation = [
-    {name: "mercury",   question: "Is this working1?", answer: "mercuryAnswer", wrongAnswers: ["wrong answer 1", "wrong answer 2", "wrong answer 3"]},
-    {name: "venus",     question: "Is this working2?", answer: "venusAnswer",   wrongAnswers: ["wrong answer 4", "wrong answer 5", "wrong answer 6"]},
-    {name: "earth",     question: "Is this working3?", answer: "earthAnswer",   wrongAnswers: ["wrong answer 7", "wrong answer 8", "wrong answer 9"]},
-    {name: "mars",      question: "Is this working4?", answer: "marsAnswer",    wrongAnswers: ["wrong answer 10", "wrong answer 11", "wrong answer 12"]},
-    {name: "jupiter",   question: "Is this working5?", answer: "jupiterAnswer", wrongAnswers: ["wrong answer 13", "wrong answer 14", "wrong answer 15"]},
-    {name: "saturn",    question: "Is this working6?", answer: "saturnAnswer",  wrongAnswers: ["wrong answer 16", "wrong answer 17", "wrong answer 18"]},
-    {name: "uranus",    question: "Is this working7?", answer: "uranusAnswer",  wrongAnswers: ["wrong answer 19", "wrong answer 20", "wrong answer 21"]},
-    {name: "neptune",   question: "Is this working8?", answer: "neptuneAnswer", wrongAnswers: ["wrong answer 22", "wrong answer 23", "wrong answer 24"]}
+    {name: "mercury",   image: "mercury-image.jpg", question: "Is this working1?", answer: "mercuryAnswer", wrongAnswers: ["wrong answer 1", "wrong answer 2", "wrong answer 3"]},
+    {name: "venus",     image: "venus-image.jpg",   question: "Is this working2?", answer: "venusAnswer",   wrongAnswers: ["wrong answer 4", "wrong answer 5", "wrong answer 6"]},
+    {name: "earth",     image: "earth-image.jpg",   question: "Is this working3?", answer: "earthAnswer",   wrongAnswers: ["wrong answer 7", "wrong answer 8", "wrong answer 9"]},
+    {name: "mars",      image: "mars-image.jpg",    question: "Is this working4?", answer: "marsAnswer",    wrongAnswers: ["wrong answer 10", "wrong answer 11", "wrong answer 12"]},
+    {name: "jupiter",   image: "jupiter-image.jpg", question: "Is this working5?", answer: "jupiterAnswer", wrongAnswers: ["wrong answer 13", "wrong answer 14", "wrong answer 15"]},
+    {name: "saturn",    image: "saturn-image.jpg",  question: "Is this working6?", answer: "saturnAnswer",  wrongAnswers: ["wrong answer 16", "wrong answer 17", "wrong answer 18"]},
+    {name: "uranus",    image: "uranus-image.jpg",  question: "Is this working7?", answer: "uranusAnswer",  wrongAnswers: ["wrong answer 19", "wrong answer 20", "wrong answer 21"]},
+    {name: "neptune",   image: "neptune-image.jpg", question: "Is this working8?", answer: "neptuneAnswer", wrongAnswers: ["wrong answer 22", "wrong answer 23", "wrong answer 24"]}
 ]
+
+const imageArray = ["/mercury-image.jpg", "/venus-image.jpg", "/earth-image.jpg", "/mars-image.jpg", "/jupiter-image.jpg", "/saturn-image.jpg", "/uranus-image.jpg", "/neptune-image.jpg",]
 
 //opening page
 questionDiv.style.visibility = 'hidden'
@@ -42,6 +45,7 @@ function welcomePageToMercury () {
 //getting from planet to planet 
 function planetToPlanet (num) {
     planet.innerText = planetInformation[num]['name']
+    planetImage.src = imageArray[num]
     question.innerText = planetInformation[num]['question']
     answer1.innerText = planetInformation[num]['answer']
     answer2.innerText = planetInformation[num]['wrongAnswers'][0]
