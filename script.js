81b8ab480aadf01a5e1ca804aba89c1c19552978
toddpeltzer <toddpeltzer14@gmail.com>
2021-08-03 14:27:42 -0700

    Working fully with a checkpoint halfway

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let welcomeDiv = document.querySelector('.welcomeSection') //set a variable for welcome div section
-let startButton = document.querySelector('.startGame') //set variable for start button
-let questionDiv = document.querySelector('.questionSection') //set variable for question div section
-let buttonDiv = document.querySelector('.buttonDiv') //set variable where answer buttons will be created
-let planet = document.querySelector('.planetName') //set variable where planet names will go
-let planetImage = document.querySelector('.planetImage') //set variable where planet images will go
-let question = document.querySelector('.planetQuestion') ////set variable where planet questions will go
+const welcomeDiv = document.querySelector('.welcomeSection') //set a variable for welcome div section
+const startButton = document.querySelector('.startGame') //set variable for start button
+const questionDiv = document.querySelector('.questionSection') //set variable for question div section
+const buttonDiv = document.querySelector('.buttonDiv') //set variable where answer buttons will be created
+const planet = document.querySelector('.planetName') //set variable where planet names will go
+const planetImage = document.querySelector('.planetImage') //set variable where planet images will go
+const question = document.querySelector('.planetQuestion') ////set variable where planet questions will go
 let counter = 0 //set variable for the counter in which our function will run through the array of objects based on correct/incorrect
 //adding rule modal elements
 const rulesModalContainer = document.querySelector('#rulesModalContainer')
@@ -21,14 +21,14 @@ const planetModalButton = document.querySelector('#planetModalButton')
 
 //planet information
 const planetInformation = [
-    {name: "Mercury",   image: "/mercury-image.png", question: "What color is Mercury's sky?",                                               answer: ["Black"],                  wrongAnswers: ["Blue", "Red", "Orange"],                                                                                            explination: "Mercury has no real atmosphere to scatter the Sun's rays, and so the sky appears black."},
-    {name: "Venus",     image: "/venus-image.png",   question: "Why is Venus often referred to as Earth's 'sister planet'?",                 answer: ["Similar size and mass"],  wrongAnswers: ["Same amount of days in a year", "They both have water on their surface", "They have the same temperature ranges"],  explination: "While Earth is slightly larger and holds more mass, if they could be viewed side by side they would appear identical."},
-    {name: "Earth",     image: "/earth-image.png",   question: "How does Earth's size compare to the other planets in our solar system?",    answer: ["Fifth-largest"],          wrongAnswers: ["Third-largest", "Fourth-largest", "Sixth-largest"],                                                                 explination: "The order from largest to smallest planet in our solar system is Jupiter, Saturn, Uranus, Neptune, Earth, Venus, Mars, and Mercury."},
-    {name: "Mars",      image: "/mars-image.png",    question: "95 percent of Mar's atmosphere is made up of?",                              answer: ["Carbon Dioxide"],         wrongAnswers: ["Oxygen", "Nitrogen", "Carbon Monoxide"],                                                                            explination: "The photochemical reactions in the atmosphere tend to oxidize the organic species and turn them into carbon dioxide."},
-    {name: "Jupiter",   image: "/jupiter-image.png", question: "How many Earth's could fit into Jupiter?",                                   answer: ["1,300"],                  wrongAnswers: ["1,800", "1,000", "1,500"],                                                                                          explination: "Jupiter is so big it could fit every other planet inside it."},
-    {name: "Saturn",    image: "/saturn-image.png",  question: "What shape is the storm located at Saturn's North Pole?",                    answer: ["Hexagon"],                wrongAnswers: ["Circle", "Oval", "Octagon"],                                                                                        explination: "This storm was spotted by Voyager 1 and confirmed on the Cassini mission. Hubble has confirmed there is no similar Southern Pole."},
-    {name: "Uranus",    image: "/uranus-image.png",  question: "What makes Uranus blue?",                                                    answer: ["Methane"],                wrongAnswers: ["Oxygen", "Nitrogen", "Carbon Dioxide"],                                                                             explination: "The methane in Uranus' upper atmosphere absorbs the red light from the sun and reflects blue."},
-    {name: "Neptune",   image: "/neptune-image.png", question: "What was Neptune named after?",                                              answer: ["God of the Sea"],         wrongAnswers: ["God of the Air", "God of the Land", "God of the Moon"],                                                             explination: "It was given this name due to its blue ocean-like color."}
+    {name: "Mercury",   image: "/images/mercury-image.png", question: "What color is Mercury's sky?",                                               answer: ["Black"],                              wrongAnswers: ["Blue", "Red", "Orange"],                                                                                                                        explination: "Mercury has no real atmosphere to scatter the Sun's rays, and so the sky appears black."},
+    {name: "Venus",     image: "/images/venus-image.png",   question: "Why is Venus often referred to as Earth's 'sister planet'?",                 answer: ["They have a similar size and mass"],  wrongAnswers: ["They have the same amount of days in a year", "They both have water on their surface", "They have the same temperature ranges"],                explination: "While Earth is slightly larger and holds more mass, if they could be viewed side by side they would appear identical."},
+    {name: "Earth",     image: "/images/earth-image.png",   question: "How does Earth's size compare to the other planets in our solar system?",    answer: ["Fifth-largest"],                      wrongAnswers: ["Third-largest", "Fourth-largest", "Sixth-largest"],                                                                                             explination: "The order from largest to smallest planet in our solar system is Jupiter, Saturn, Uranus, Neptune, Earth, Venus, Mars, and Mercury."},
+    {name: "Mars",      image: "/images/mars-image.png",    question: "95 percent of Mar's atmosphere is made up of?",                              answer: ["Carbon Dioxide"],                     wrongAnswers: ["Oxygen", "Nitrogen", "Carbon Monoxide"],                                                                                                        explination: "The photochemical reactions in the atmosphere tend to oxidize the organic species and turn them into carbon dioxide."},
+    {name: "Jupiter",   image: "/images/jupiter-image.png", question: "How many Earth's could fit into Jupiter?",                                   answer: ["1,300"],                              wrongAnswers: ["1,800", "1,000", "1,500"],                                                                                                                      explination: "Jupiter is so big it could fit every other planet inside it."},
+    {name: "Saturn",    image: "/images/saturn-image.png",  question: "What shape is the storm located at Saturn's North Pole?",                    answer: ["Hexagon"],                            wrongAnswers: ["Circle", "Oval", "Octagon"],                                                                                                                    explination: "This storm was spotted by Voyager 1 and confirmed on the Cassini mission. Hubble has confirmed there is no similar Southern Pole."},
+    {name: "Uranus",    image: "/images/uranus-image.png",  question: "What makes Uranus blue?",                                                    answer: ["Methane"],                            wrongAnswers: ["Oxygen", "Nitrogen", "Carbon Dioxide"],                                                                                                         explination: "The methane in Uranus' upper atmosphere absorbs the red light from the sun and reflects blue."},
+    {name: "Neptune",   image: "/images/neptune-image.png", question: "What was Neptune named after?",                                              answer: ["God of the Sea"],                     wrongAnswers: ["God of the Air", "God of the Land", "God of the Moon"],                                                                                         explination: "It was given this name due to its blue ocean-like color."}
 ]
 
 //function to get to mercury from home page
@@ -71,7 +71,7 @@ function planetToPlanet (num) { //num will be the counter, which starts at 0. We
                 buttonDiv.style.display = "none" //bakes the buttons that were created disappear
                 planetModalContainer.style.display = 'block' //has the in game modal appear
                 planetModalHeader.innerText = "You successfully made it out of the solar system!" //creates inner text within the in game modal
-                planetModalImage.src="escaped-image.gif" //adds an image to the in game modal
+                planetModalImage.src="/gifs/escaped-image.gif" //adds an image to the in game modal
                 planetModalFact.innerText = "What now?...." //adds more text under the image source within the in game modal
                 planetModalButton.innerText = "Go Home?" //adds text to the button in the in game modal
                 //add event listener for when the button is clicked
@@ -84,22 +84,25 @@ function planetToPlanet (num) { //num will be the counter, which starts at 0. We
                     welcomeDiv.style.display = 'block'
                 })
             //if the inner text of what was just clicked equals the answer, then the next planet screen appears
+            } else if (event.target.innerText === planetInformation[3]['answer'][0]) {
+                questionDiv.style.display = "none"
+                buttonDiv.style.display = "none"
+                planetModalContainer.style.display = 'block'
+                planetModalHeader.innerText = `Congratulations, you made it to ${planetInformation[num+1]['name']}! This is your checkpoint for making it to the first gas giant.`
+                planetModalImage.src="/gifs/checkpoint-notification.gif"
+                planetModalFact.innerText = `Answer explination: ${planetInformation[num]['explination']}`
+                planetModalButton.innerText = "Next Planet"
+            //if inner text does not match the answer then the crash screen appears
             } else if (event.target.innerText === planetInformation[counter]['answer'][0]) {
                 questionDiv.style.display = "none"
                 buttonDiv.style.display = "none"
                 planetModalContainer.style.display = 'block'
                 planetModalHeader.innerText = `Nice job! You have passed ${planetInformation[num]['name']}!`
-                planetModalImage.src="next-level.gif"
-                planetModalFact.innerText = planetInformation[num]['explination']
+                planetModalImage.src="/gifs/next-level.gif"
+                planetModalFact.innerText = `Answer explination: ${planetInformation[num]['explination']}`
                 planetModalButton.innerText = "Next Planet"
-                planetModalButton.addEventListener('click', () => {
-                    buttonDiv.innerText = ""
-                    counter++
-                    planetModalContainer.style.display = 'none'
-                    planetToPlanet(counter)
-                })
             //if inner text does not match the answer then the crash screen appears
-            } else if (event.target.innerText !== planetInformation[counter]['answer'][0]) {
+            } else if ((event.target.innerText !== planetInformation[counter]['answer'][0]) && (num < 4)) {
         //ADD IN THE LIVES AND BUTTON DISABLE HERE as an 'if' statement        
                 questionDiv.style.display = "none"
                 buttonDiv.style.display = "none"
@@ -108,18 +111,28 @@ function planetToPlanet (num) { //num will be the counter, which starts at 0. We
         //reset lives on checkpoint      
                 planetModalContainer.style.display = 'block'
                 planetModalHeader.innerText = "Oh no! You crashed!"
-                planetModalImage.src="crash-image.gif"
+                planetModalImage.src="/gifs/crash-image.gif"
                 planetModalFact.innerText = ""
                 planetModalButton.innerText = "Restart your journey"
-                planetModalButton.addEventListener('click', () => {
-                    buttonDiv.innerText = ""
-                    counter = 0
-                    planetModalContainer.style.display = 'none'
-                    planetToPlanet(counter)
-                })
-            }
+                counter = -1 //hack from Will
+            } else if ((event.target.innerText !== planetInformation[counter]['answer'][0]) && (num >= 4)) {
+                        questionDiv.style.display = "none"
+                        buttonDiv.style.display = "none"     
+                        planetModalContainer.style.display = 'block'
+                        planetModalHeader.innerText = "Don't worry, you get to start at your checkpoint!"
+                        planetModalImage.src="/gifs/checkpoint.gif"
+                        planetModalFact.innerText = ""
+                        planetModalButton.innerText = "Back to Jupiter"
+                        counter = 3 //hack from Will
+                    }
         })
         buttonDiv.appendChild(elementButton) //appending the buttons to the button div
+        //answer button styling
+        buttonDiv.style.display = 'flex-wrap'
+        buttonDiv.style.justifyContent = 'center'
+        elementButton.style.margin = '20px'
+        elementButton.style.width = '200px'
+        elementButton.style.height = '50px'
     })
 }
 
@@ -128,3 +141,11 @@ startButton.addEventListener('click', welcomePageToMercury) //button to start th
 rulesButton.addEventListener('click', ruleScreen) //button to show the rules from the home page
 
 ruleCloseButton.addEventListener('click', closeRuleModal) //button to close the rules modal on the home page
+
+//event listener for when the next question button is clicked in the in game modal
+planetModalButton.addEventListener('click', () => {
+    buttonDiv.innerText = ""
+    counter++
+    planetModalContainer.style.display = 'none'
+    planetToPlanet(counter)
+})
